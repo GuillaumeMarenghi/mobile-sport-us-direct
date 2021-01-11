b9069db29212e9f46ef34bfb08101ed655683c99
guillaume Marenghi <guil.marenghi@gmail.com>
2021-01-11 17:59:39 +0100

    nba table done

--- src/components/schedulesEpic/nba/index.js
+++ src/components/schedulesEpic/nba/index.js
@@ -12,7 +12,7 @@ const Nba = () => {
 
     const east = [134863, 134862, 134883, 134860, 134861, 134870, 134871, 134880, 134881, 134873, 134874, 134882, 134872, 134864, 134884];
     const table = useSelector(getStoreSchedules);
-    console.log('table:', table.NBArank)
+    console.log('table:', typeof table.NBArank)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -30,9 +30,20 @@ const Nba = () => {
                         <DataTable.Title>Team</DataTable.Title>
                         <DataTable.Title numeric>Joué</DataTable.Title>
                         <DataTable.Title numeric>Win</DataTable.Title>
-                        <DataTable.Title numeric>Loose</DataTable.Title>
+                        <DataTable.Title numeric>Loss</DataTable.Title>
                         <DataTable.Title numeric>%</DataTable.Title>
                     </DataTable.Header>
+                    {table.NBArank.map((elm, index) => {
+                        const prc = elm.win/elm.played;
+                    return (
+                        <DataTable.Row>
+                        <DataTable.Cell>{index + 1}</DataTable.Cell>
+                        <DataTable.Cell>{elm.name}</DataTable.Cell>
+                        <DataTable.Cell numeric>{elm.played}</DataTable.Cell>
+                        <DataTable.Cell numeric>{elm.win}</DataTable.Cell>
+                        <DataTable.Cell numeric>{elm.loss}</DataTable.Cell>
+                        <DataTable.Cell numeric>{prc.toFixed(2)}</DataTable.Cell>
+                    </DataTable.Row>)})} 
                 </DataTable>
             </View>}
 
